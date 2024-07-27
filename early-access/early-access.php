33485fae1ec019f33b9e59cecc242009b6c61de8
Zagaz <andre_ranulfo@hotmail.com>
2024-07-27 20:16:46 -0300

    chore: Add custom WP CLI job to untag early access classes

--- early-access/early-access.php
+++ early-access/early-access.php
@@ -12,9 +12,50 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Requires PHP: 7.0
  */
+     if ( ! defined( 'ABSPATH' ) ) {
+          die; 
+      }
 
-//Thou shall not pass!!!
-// Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
-     exit;
+     if ( ! defined( 'WPINC' ) ) {
+          die; 
+      }
+
+      /* 
+
+      Task
+Please create a custom WP CLI job the does the following things
+
+For all the product content that has custom taxonomy product_cat term early-access remove the early-access term if the post meta public_access_date has past.
+
+Templates
+Below is the skeleton class template for the custom cli. Add your function in this class.
+This is the template:
+
+class Product_CLI extends \WP_CLI_Command {
+
+	//*
+	// Untag all early access classes when public access date is past.
+	//
+	// Your comments go here.
+	///
+	// Your function goes here.
 }
+
+WP_CLI::add_command( 'product', 'Product_CLI' );
+
+      */
+
+      class Product_CLI extends \WP_CLI_Command {
+
+          /**
+           * Untag all early access classes when public access date is past.
+           */
+          public function untag_early_access() {
+              // To:do The function will be implemented here.
+          }
+      }
+
+          WP_CLI::add_command( 'product', 'Product_CLI' );
+
+          
+      
