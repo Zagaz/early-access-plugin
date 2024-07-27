085b9df8f6558380e8c522d37424fe95eee13e8c
Zagaz <andre_ranulfo@hotmail.com>
2024-07-27 19:58:08 -0300

    chore: Add plugin header and exit condition to early-access.php

--- early-access/early-access.php
+++ early-access/early-access.php
@@ -1,3 +1,20 @@
 <?php 
+/**
+ * Plugin Name: Early Access
+ * Description: A custom WP CLI command to untag early access products.
+ * Version: 1.0.0
+ * Author: Andre Ranulfo
+ * Author URI: https://www.linkedin.com/in/andre-ranulfo/
+ * Plugin URI: https://github.com/Zagaz/early-access-plugin
+ * Text Domain: early-access
+ * Domain Path: /lang
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Requires PHP: 7.0
+ */
 
-echo "Hello World";
+//Thou shall not pass!!!
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+     exit;
+}
